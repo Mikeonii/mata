1e5d4d5bb03a2a13ce9ec453b964dc5d18a3ec1d
Mikeonii <janmichael_0123@outlook.com>
2021-03-26 10:53:04 +0800

    another fucked

--- resources/views/print_summary.blade.php
+++ resources/views/print_summary.blade.php
@@ -12,5 +12,6 @@
 	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 	{{-- sample fuck --}}
+	{{-- another fucked --}}
 </body>
 </html>
